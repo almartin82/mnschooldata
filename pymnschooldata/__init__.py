e96b2f65dd3e7b679985c1811b0fa4d94377a1c1
almartin82 <almartin@gmail.com>
2025-12-31 20:11:18 -0500

    Add Python wrapper package

---
+++ pymnschooldata/__init__.py
@@ -0,0 +1,16 @@
+"""
+pymnschooldata - Python wrapper for Minnesota school enrollment data.
+
+Thin rpy2 wrapper around the mnschooldata R package.
+Returns pandas DataFrames.
+"""
+
+from .core import (
+    fetch_enr,
+    fetch_enr_multi,
+    tidy_enr,
+    get_available_years,
+)
+
+__version__ = "0.1.0"
+__all__ = ["fetch_enr", "fetch_enr_multi", "tidy_enr", "get_available_years"]
